d2ca61075f8b18e3541e6da57906e51996da95e7
ttbarnes <tony@tonybarnes.me>
2017-07-10 17:14:32 +0100

    server running

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var app = express();
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var port = process.env.PORT || 2000;
 
+app.use(function (req, res, next){
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  methodOverride('X-HTTP-Method-Override');
+  bodyParser.urlencoded({ extended: true });
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.listen(port);
+console.log('Server running on port ' + port);
